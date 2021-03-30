757c9bf2e39cdb594969c56db19ba7b9e3324e9b
gqc954 <ivan.vogel@gmail.com>
2021-03-30 15:13:03 +0200

    optimized calculate_ma

--- R/calculate_ma.R
+++ R/calculate_ma.R
@@ -1,4 +1,8 @@
 calculate_ma = function(df) {
- df$calculate_transformations_2()  
- re <- df
+   np=import('numpy')
+  df %>%
+    mutate(m=np$log2(x) -  np$log2(y),
+           a=0.5 * (np$log2(x) + np$log2(y)),
+           m_raw=np$log2(x_raw) -  np$log2(y_raw),
+           a_raw=0.5 * (np$log2(x_raw) + np$log2(y_raw)))
 }
