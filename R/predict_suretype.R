e783993758f89ff3449cb12115dadf20e25a07bf
gqc954 <ivan.vogel@gmail.com>
2021-04-13 23:02:00 +0200

    fixed bug in predict_suretype

--- R/predict_suretype.R
+++ R/predict_suretype.R
@@ -2,7 +2,7 @@ predict_suretype <- function(.data,rf_clf)
 {
   .data %>%
     calculate_ma() %>%
-    scpredict(clf_rf,clftype='rf') %>% #1st layer
+    scpredict(rf_clf,clftype='rf') %>% #1st layer
     scpredict(scTrain(create_dataobject_from_frame(.),clfname='gda'),clftype='rf-gda') %>%
     select(!c('rf-gda_ratio:1.0_pred','rf_ratio:1.0_pred'))  %>%
     rename('rf-gda_score'='rf-gda_ratio:1.0_prob',
