4bdb641d3d610d08e531f3a82e612692dfd8ccc2
gqc954 <ivan.vogel@gmail.com>
2021-05-06 12:23:01 +0200

    added metadata paramater to plot_pca

--- R/plot_pca.R
+++ R/plot_pca.R
@@ -7,46 +7,51 @@ anfun <- function(.x)
 }
 
 
-plot_pca <- function(.data,by_chrom=TRUE,features=c('x','y'),labels=TRUE)
+plot_pca <- function(.data,by_chrom=TRUE,features=c('x','y'),labels=TRUE,metadata=NULL)
 {
   pallette=colorRampPalette(brewer.pal(8, "Set2"))(length(unique(.data$individual)))
   if (by_chrom==TRUE)
   {
-  p=.data %>%
-    as_tibble() %>%
-    group_by(Chr) %>% 
-    nest() %>%
-    mutate(pca_preproc = map(data, ~ preprocess_pca(.x,.y,feat=features)),
-           pca = map(pca_preproc,~ anfun(.x)),
-           imp_pc1=map(pca,~summary(.x)$importance['Proportion of Variance','PC1'] ),
-           imp_pc2=map(pca,~summary(.x)$importance['Proportion of Variance','PC2'] ),
-           pca_df=map(pca,~{as.data.frame(.x$x)}),
-           pca_df=map(pca_df,~mutate(.x,individual=rownames(.x)))) %>%
-    select(Chr,imp_pc1,imp_pc2,pca_df) %>%
-    unnest(c(pca_df,imp_pc1,imp_pc2))  %>%
-    mutate(imp_pc1=round(imp_pc1,2),
-           imp_pc2=round(imp_pc2,2))
-
-  
-  labels_df=p %>% select(Chr,imp_pc1,imp_pc2) %>% mutate(label=str_c('PC1:',imp_pc1,'PC2: ', imp_pc2,sep=' '))  %>% distinct() %>% select(Chr,label)  
-  
-  plot=ggplot(data=p)  + 
-    geom_jitter(aes(x=PC1,y=PC2,shape=factor(individual),color=factor(individual))) + 
-    facet_wrap(~Chr,scales = 'free') + 
-    theme_bw() +  
-    scale_color_manual(values=pallette) + 
-    scale_shape_manual(values = 0:(length(unique(.data$individual))-1)) + 
-    labs(color = "Individual") 
-  
-  if(labels==TRUE)
-  {
-    plot=plot+geom_text(data=labels_df,mapping = aes(x = -Inf, y = -Inf, label = label), hjust   = -0.1,vjust   = -1)
-  }
+    p=.data %>%
+      as_tibble() %>%
+      group_by(Chr) %>% 
+      nest() %>%
+      mutate(pca_preproc = map(data, ~ preprocess_pca(.x,.y,feat=features)),
+             pca = map(pca_preproc,~ anfun(.x)),
+             imp_pc1=map(pca,~summary(.x)$importance['Proportion of Variance','PC1'] ),
+             imp_pc2=map(pca,~summary(.x)$importance['Proportion of Variance','PC2'] ),
+             pca_df=map(pca,~{as.data.frame(.x$x)}),
+             pca_df=map(pca_df,~mutate(.x,individual=rownames(.x)))) %>%
+      select(Chr,imp_pc1,imp_pc2,pca_df) %>%
+      unnest(c(pca_df,imp_pc1,imp_pc2))  %>%
+      mutate(imp_pc1=round(imp_pc1,2),
+             imp_pc2=round(imp_pc2,2))
+    
+    
+    labels_df=p %>% select(Chr,imp_pc1,imp_pc2) %>% mutate(label=str_c('PC1:',imp_pc1,'PC2: ', imp_pc2,sep=' '))  %>% distinct() %>% select(Chr,label)  
+    
+    
+    if (!is.null(metadata)) {
+      p %<>% left_join(metadata,by=c('individual'='id'))
+    }
+    
+    plot=ggplot(data=p,aes(x=PC1,y=PC2,color=factor(familyid),label=individual))  + 
+      geom_jitter() + 
+      facet_wrap(~Chr,scales = 'free') + 
+      theme_bw() +  
+      #scale_color_manual(values=pallette) + 
+      #scale_shape_manual(values = 0:(length(unique(.data$individual))-1)) + 
+      #labs(color = "Individual") 
+      
+      if(labels==TRUE)
+      {
+        plot=plot+geom_text(data=labels_df,mapping = aes(x = -Inf, y = -Inf, label = label), hjust   = -0.1,vjust   = -1)
+      }
   }
   else
   {
     p=.data %>%
-    as_tibble() %>%
+      as_tibble() %>%
       nest(data=everything()) %>%
       mutate(pca_preproc = map(data, ~ preprocess_pca(.x,.y,feat=features)),
              pca = map(pca_preproc,~ anfun(.x)),
@@ -61,17 +66,23 @@ plot_pca <- function(.data,by_chrom=TRUE,features=c('x','y'),labels=TRUE)
     
     labels_df=p %>% select(imp_pc1,imp_pc2) %>% mutate(label=str_c('PC1:',imp_pc1,'PC2: ', imp_pc2,sep=' '))  %>% distinct() %>% select(label)  
     
-    plot=ggplot(data=p)  + 
-      geom_jitter(aes(x=PC1,y=PC2,shape=factor(individual),color=factor(individual))) + 
-                    theme_bw() +  
-      scale_color_manual(values=pallette) + 
-      scale_shape_manual(values = 0:(length(unique(.data$individual))-1)) + 
-      labs(color='Individual') 
-    
-    if(labels==TRUE)
-    {
-     plot=plot+geom_text(data=labels_df,mapping = aes(x = -Inf, y = -Inf, label = label), hjust   = -0.1,vjust   = -1)
+    if (!is.null(metadata)) {
+      p %<>% left_join(metadata,by=c('individual'='id'))
     }
+    
+    
+    plot=ggplot(data=p,aes(x=PC1,y=PC2,color=factor(familyid),label=individual))  + 
+      geom_jitter() + 
+      theme_bw() +  
+      #scale_color_manual(values=pallette) + 
+      #scale_shape_manual(values = 0:(length(unique(.data$individual))-1)) + 
+      #labs(color='Individual') 
+      
+      if(labels==TRUE)
+      {
+        plot=plot+geom_text(data=labels_df,mapping = aes(x = -Inf, y = -Inf, label = label), hjust   = -0.1,vjust   = -1)
+      }
   }
   return(plot)
 }
+
