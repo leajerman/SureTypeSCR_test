8a85ae4f79971b386dea4b108ccfc97cb35bf2e4
gqc954 <ivan.vogel@gmail.com>
2021-05-07 13:04:59 +0200

    scbasic bug

--- R/scbasic.R
+++ R/scbasic.R
@@ -2,9 +2,9 @@
 
 scbasic = function(bpm,egt,samplesheet) {
   vec=c('samplesheet','manifest','cluster')[!file.exists(c(samplesheet,bpm,egt))]
-  stop(paste('Cannot locate',paste(vec,collapse=',')))
- re <- scEls()$sc$basic(bpm,egt,samplesheet)
- re <- scEls()$sc$Data$create_from_frame(re)
- re <- get_simpleind_df(re)
- #re <- py_to_r(re)
+  if (sum(vec)!=0) stop(paste('Cannot locate',paste(vec,collapse=',')))
+  re <- scEls()$sc$basic(bpm,egt,samplesheet)
+  re <- scEls()$sc$Data$create_from_frame(re)
+  re <- get_simpleind_df(re)
+  #re <- py_to_r(re)
 }
