cd45be7cc9f676c51ba6869578d0c9217de17a6b
gqc954 <ivan.vogel@gmail.com>
2021-05-07 12:51:39 +0200

    scbasic error checking, sample data

--- R/scbasic.R
+++ R/scbasic.R
@@ -1,6 +1,8 @@
 #'interface to basic function in SureTypeSC 
 
 scbasic = function(bpm,egt,samplesheet) {
+  vec=c('samplesheet','manifest','cluster')[!file.exists(c(samplesheet,bpm,egt))]
+  stop(paste('Cannot locate',paste(vec,collapse=',')))
  re <- scEls()$sc$basic(bpm,egt,samplesheet)
  re <- scEls()$sc$Data$create_from_frame(re)
  re <- get_simpleind_df(re)
