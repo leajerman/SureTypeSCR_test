dc21b93af2a07b148fca03fc1de4a9e06a4e1b41
gqc954 <ivan.vogel@gmail.com>
2021-05-07 13:13:56 +0200

    scbasic

--- R/scbasic.R
+++ R/scbasic.R
@@ -2,7 +2,7 @@
 
 scbasic = function(bpm,egt,samplesheet) {
   vec=c('samplesheet','manifest','cluster')[!file.exists(c(samplesheet,bpm,egt))]
-  if (sum(vec)!=0) stop(paste('Cannot locate',paste(vec,collapse=',')))
+  if (length(vec)!=0) stop(paste('Cannot locate',paste(vec,collapse=',')))
   re <- scEls()$sc$basic(bpm,egt,samplesheet)
   re <- scEls()$sc$Data$create_from_frame(re)
   re <- get_simpleind_df(re)
