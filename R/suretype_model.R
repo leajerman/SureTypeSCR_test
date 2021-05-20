32c6b38453f2ca1ad548de16bef84296b16ff5ab
gqc954 <ivan.vogel@gmail.com>
2021-05-20 10:35:45 +0200

    model update

--- R/suretype_model.R
+++ R/suretype_model.R
@@ -1,22 +1,24 @@
-suretype_model <- function(.data,.individual,.sclist='all',rf_clf)
+suretype_model <- function(.data,.individual,rf_clf, .sclist='all')
 {
-  print(.individual)
+  #print(.individual)
   #print(.sclist)
   #print(.data)
   if (.sclist=='all' | .individual %in% .sclist)
   {
+    write(paste("Processing sample",.individual,sep=' '), stdout())
     .data %>% mutate(individual=.individual) %>%
-      scpredict(rf_clf,clftype='rf') %>% #1st layer
+      scpredict(scload(rf_clf),clftype='rf') %>% #1st layer
       scpredict(scTrain(create_dataobject_from_frame(.),clfname='gda'),clftype='rf-gda') %>%
-      rename('rfgda.score'='rf-gda_ratio:1.0_prob',
-             'rf.score'='rf_ratio:1.0_prob') %>%
-      select('rfgda.score','rf.score')  %>%
+      rename('rfgda_score'='rf-gda_ratio:1.0_prob',
+             'rf_score'='rf_ratio:1.0_prob') %>%
+      select('rfgda_score','rf_score')  %>%
       as_tibble()
+    write('-------------------', stdout())
   }
   else
   {
     .data %>% 
-      mutate('rfgda.score'=1,'rf.score'=1) %>%
-      select('rfgda.score','rf.score')  
+      mutate('rfgda_score'=1,'rf_score'=1) %>%
+      select('rfgda_score','rf_score')  
   }
 }
